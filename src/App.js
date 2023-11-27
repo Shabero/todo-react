dea5843f3fca66b102d05ee333b77f96234d64e7
Shabdan <tokmokiuca@gmail.com>
2023-11-27 13:05:52 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import dayjs from "dayjs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const url = 'https://65642481ceac41c0761d7eb6.mockapi.io/todo'
+
+const App = () => {
+    const [todos, setTodos] = useState([])
+    const [todoTitle, setTodoTitle] = useState('')
+
+    useEffect(() => {
+        axios(url)
+            .then(({data}) => setTodos(data))
+    },[])
+
+    const handleAddTodo = () => {
+        const newTodo = {
+            title: todoTitle,
+            completed: false,
+            completedAt: null,
+            createdAt: +new Date()
+        }
+        setTodoTitle('')
+            axios.post(url, newTodo)
+                .then(({data}) => setTodos([...todos, data]))
+    }
+
+    // const handleDelete  = (todo) => {
+    //     axios.delete(`${url}/ ${todo.id}` )
+    // }
+
+    return (
+        <div className={'container'}>
+          <h1 className={'title'}>Todo list</h1>
+            <input type="text" onChange={(e) => setTodoTitle(e.target.value)} value={todoTitle}/>
+            <button onClick={handleAddTodo}>Add todo</button>
+            {
+                todos.map(todo =>
+                <div key={todo.id} className={'todo-wrapper'}>
+                    <p>{todo.title}</p>
+                    <input type="checkbox" checked={todo.completed}/>
+                    <span>{dayjs(todo.createdAt).format(' HH.mm DD.MM.YYYY')}</span>
+                    {/*<button onClick={() => handleDelete(todo)}>Delete Todo</button>*/}
+                </div>
+                )
+            }
+        </div>
+    );
+};
 
 export default App;
